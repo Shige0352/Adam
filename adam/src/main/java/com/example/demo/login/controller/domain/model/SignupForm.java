b6bc9a5607755dd7d19f2157a428a2dc0210738a
Shige0352 <shigeaki52@gmail.com>
2019-02-20 00:56:53 +0900

    SignuoFrom.javaにバリデーション追加

--- adam/src/main/java/com/example/demo/login/controller/domain/model/SignupForm.java
+++ adam/src/main/java/com/example/demo/login/controller/domain/model/SignupForm.java
@@ -2,6 +2,14 @@ package com.example.demo.login.controller.domain.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -9,12 +17,33 @@ import lombok.Data;
 @Data
 public class SignupForm {
 
+	//必須入力、メールアドレス形式
+	@NotBlank
+	@Email
 	private String userId;
+
+	//必須入力、長さ4~100桁まで、半角英数字のみ
+	@NotBlank
+	@Length(min = 4, max = 100)
+	@Pattern(regexp= "^[a-zA-Z0-9]+$")
 	private String password;
+
+	//必須入力
+	@NotBlank
 	private String userName;
 
+
+	//必須入力
+	@NotBlank
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private Date birthday;
+
+	//値が20から100まで
+	@Min(20)
+	@Max(100)
 	private int age;
+
+	//falseのみ可能
+	@AssertFalse
 	private boolean marriage;
 }
